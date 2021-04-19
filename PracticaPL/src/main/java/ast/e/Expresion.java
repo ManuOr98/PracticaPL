9643993268f68760e10defc26cec355c9e354fba
Manuel Ortega Salvador <33860075+ManuOr98@users.noreply.github.com>
2021-04-19 12:00:14 +0200

    Delete PracticaPL directory

--- PracticaPL/src/main/java/ast/e/Expresion.java
+++
@@ -1,8 +0,0 @@
-package ast.e;
-
-public abstract class Expresion {
-   public abstract Expresiones tipo(); 
-   public Expresion opnd1() {throw new UnsupportedOperationException("opnd1");} 
-   public Expresion opnd2() {throw new UnsupportedOperationException("opnd2");} 
-   public String num() {throw new UnsupportedOperationException("num");}
-}
--- PracticaPL/src/main/java/ast/i/Instruccion.java
+++
@@ -1,7 +0,0 @@
-package ast.i;
-
-public abstract class Instruccion {
-    public Instruccion(){}
-    public abstract String toString();
-    public abstract TipoInstruccion getTipo();
-}
